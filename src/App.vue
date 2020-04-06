028f60a08e6ff4f35ada71f336604da9e566a6e5
zhaoxunyin <17603016347@163.com>
2020-04-06 18:42:56 +0800

    创建vue_elementui脚手架

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <el-button type="success">中国人民解放君</el-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import{Button} from "element-ui"
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(Button);
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
